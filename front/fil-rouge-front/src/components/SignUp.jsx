439c2a877817ec5fb5062ace3bb13477f767405a
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-01-03 16:12:25 +0100

    quete_020

--- front/fil-rouge-front/src/components/SignUp.jsx
+++ front/fil-rouge-front/src/components/SignUp.jsx
@@ -3,18 +3,55 @@ import React, { Component } from "react";
 export default class SignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "changer le titre" };
+    this.state = {
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      name: "James",
+      lastname: "Bond",
+    };
     this.handleInput = this.handleInput.bind(this);
   }
-  handleInput(event) {
-    this.setState({ title: event.target.value });
+  handleInput(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventdefault();
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div className='SignupComponent'>
-        <h1>{this.state.title}</h1>
-        <input type='email' name='email' onChange={this.handleInput} />
+      <div>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type='firstname'
+            name='firstname'
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input
+            type='lastname'
+            name='lastname'
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input
+            type='email'
+            name='email'
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input
+            type='password'
+            name='password'
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input
+            type='passwordConfirmation'
+            name='passwordConfirmation'
+            onChange={(e) => this.handleInput(e)}
+          />
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }
