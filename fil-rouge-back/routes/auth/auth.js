61b310478e6025e35df1caa839af470f129d5942
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-01-05 14:20:32 +0100

    quete_040

--- fil-rouge-back/routes/auth/auth.js
+++ fil-rouge-back/routes/auth/auth.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const connection = require("../../helpers/db");
 
-router.post("/signup", (req, res) => {
-  res.send("I am in POST signup");
+router.post("/signup", (req, res, next) => {
+  const { email, password, name, lastname } = req.body;
+  connection.query(
+    "INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)",
+    [email, password, name, lastname],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error post");
+      } else {
+        res.status(200).send("Post ok");
+      }
+    }
+  );
 });
 
 module.exports = router;
